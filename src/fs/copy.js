2929070fd3402ba37ef7cbe3da2e99e2541d8e44
Pas92 <pavelburdovitsin92@gmail.com>
2022-11-30 13:41:51 +0300

    feat: add solution for fs:copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import * as fs from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+  const copiesFolder = path.join(__dirname, "files_copy");
+  try {
+    await fs.mkdir(copiesFolder);
+    await fs.cp(path.join(__dirname, "files"), copiesFolder, {
+      recursive: true,
+    });
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 copy();
