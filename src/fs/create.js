2d53dbd67c748b1a6716f6ddf1504902d4db7316
Pas92 <pavelburdovitsin92@gmail.com>
2022-11-30 16:21:42 +0300

    feat: add solution for fs:create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import * as fs from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const targetFolder = path.join(__dirname, "files");
+  const content = "I am fresh and young";
+  try {
+    await fs.open(path.join(targetFolder, "fresh.txt"), "wx");
+    await fs.appendFile(path.join(targetFolder, "fresh.txt"), content);
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
