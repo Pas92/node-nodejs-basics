8298a8da6990df16ae7f4332687dac10935b5c65
Pas92 <pavelburdovitsin92@gmail.com>
2022-12-02 16:52:44 +0300

    feat: add solution for streams: transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,26 @@
+import path from 'node:path';
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+class ReverseTransform extends Transform {
+  _transform(chunk, enc, cb) {
+    try {
+      cb(
+        null,
+        chunk.toString().trim().split('').reverse().join('').concat('\n')
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
 const transform = async () => {
-    // Write your code here 
+  pipeline(process.stdin, new ReverseTransform(), process.stdout);
 };
 
 await transform();
