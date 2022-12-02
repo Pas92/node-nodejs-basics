c5e222ce7ba0374a605a818643496862ddf876e8
Pas92 <pavelburdovitsin92@gmail.com>
2022-12-02 15:52:36 +0300

    feat: add solution for hash: calcHash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+
+const { createHash } = await import('node:crypto');
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+  const input = createReadStream(
+    path.join(__dirname, 'files', 'fileToCalculateHashFor.txt')
+  );
+  input.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
