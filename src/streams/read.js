6abc1d7760b92e15285abd0ccc0d9195f48c988f
Pas92 <pavelburdovitsin92@gmail.com>
2022-12-02 16:19:24 +0300

    feat: add solution for streams: read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { createReadStream } from 'node:fs';
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
+import { pipeline } from 'node:stream/promises';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const rs = createReadStream(path.join(__dirname, 'files', 'fileToRead.txt'));
+  pipeline(rs, process.stdout).catch((err) => {
+    throw new Error('Stream error');
+  });
 };
 
 await read();
