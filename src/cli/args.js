af778f73411f65c39d2b5f8b1db9a7c9f267cec3
Pas92 <pavelburdavitsyn92@gmail.com>
2022-12-03 18:18:32 +0300

    feat: add solution for cli: args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const parseArgs = () => {
-    // Write your code here 
+  const keys = process.argv.filter((el, i) => i % 2 === 0).slice(1);
+  const values = process.argv.filter((el, i) => i % 2 !== 0).slice(1);
+
+  keys.forEach((key, i) => {
+    console.log(`${key.slice(2)} is ${values[i]};`);
+  });
 };
 
 parseArgs();
