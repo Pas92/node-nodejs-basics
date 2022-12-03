73a279076fd9e29ad7b54d8d8afe5deebaa2905f
Pas92 <pavelburdavitsyn92@gmail.com>
2022-12-03 23:01:00 +0300

    feat: add solution for cp: cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,27 @@
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { fork } from 'node:child_process';
+import { stdin } from 'node:process';
+import { pipeline } from 'node:stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const controller = new AbortController();
+  const { signal } = controller;
+  const { silent } = true;
+  const child = fork(path.join(__dirname, 'files', 'script.js'), args || [], {
+    signal,
+    silent,
+  });
+
+  child.on('error', (err) => {
+    console.log(err);
+    controller.abort();
+    throw err;
+  });
 };
 
 spawnChildProcess();
