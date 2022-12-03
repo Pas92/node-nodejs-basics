7ed14b2afb255e15edd6f0a3b63300ed1e862dc4
Pas92 <pavelburdavitsyn92@gmail.com>
2022-12-03 18:04:08 +0300

    feat: add solution for cli: env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
+import * as fs from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const parseEnv = () => {
-    // Write your code here 
+  const keys = Object.keys(process.env).filter((name) => name.includes("RSS_"));
+
+  for (const key of keys) {
+    console.log(`${key}=${process.env[key]};`);
+  }
 };
 
 parseEnv();
