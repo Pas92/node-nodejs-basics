0591714d700d4ba7602b5e0f441b488b0c3f8b59
Pas92 <pavelburdavitsyn92@gmail.com>
2022-12-04 18:20:23 +0300

    feat: add solution for zlib tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import * as fs from 'node:fs/promises';
+import path from 'node:path';
+import { pipeline } from 'node:stream/promises';
+import { fileURLToPath } from 'node:url';
+import { createGzip } from 'node:zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  const source = createReadStream(
+    path.join(__dirname, 'files', 'fileToCompress.txt')
+  );
+
+  const destination = createWriteStream(
+    path.join(__dirname, 'files', 'archive.gz')
+  );
+
+  pipeline(source, gzip, destination).catch(() => {
+    throw new Error('Compress operation failed');
+  });
 };
 
 await compress();
